bdac65a0582128ec0d11c54d8927350e3687cd3c
nicolasouzazz <99694426+nicolasouzazz@users.noreply.github.com>
2025-11-25 17:50:42 -0300

    Primeiro commit

---
+++ EstruturaCondicional/src/App.java
@@ -0,0 +1,55 @@
+
+//import java.time.LocalDate;
+//import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Exemplo de estrutura condicional Media de Aluno
+         * 
+         * Scanner scanner = new Scanner(System.in);
+         * System.out.println("Cálculo de Média Escolar");
+         * System.out.println("=======================");
+         * System.out.print("Digite o nome do aluno: ");
+         * String nome = scanner.nextLine();
+         * System.out.print("Digite a primeira nota: ");
+         * float n1 = scanner.nextFloat();
+         * System.out.print("Digite a segunda nota: ");
+         * float n2 = scanner.nextFloat();
+         * float media = (n1 + n2) / 2;
+         * 
+         * if (media >= 7) {
+         * System.out.println("Média do aluno  " + nome + ":" + media +
+         * " Aprovado Parabéns!");
+         * } else {
+         * System.out.println("Média do aluno " + nome + ":" + media +
+         * " Reprovado Recuperação!");
+         * }
+         * scanner.close();
+         */
+
+        /*
+         * // Exemplo de estrutura condicional Maior de idade
+         * 
+         * Scanner scanner = new Scanner(System.in);
+         * LocalDate ano = LocalDate.now();
+         * System.out.println("Verificador de maioridade");
+         * System.out.println("=========================");
+         * System.out.print("Digite seu nome:");
+         * String nome = scanner.nextLine();
+         * System.out.print("Digite o ano de nascimento: ");
+         * int anoNascimento = scanner.nextInt();
+         * int idade = ano.getYear() - anoNascimento;
+         * 
+         * // int anoAtual = 2025;
+         * // int idade = anoAtual - anoNascimento;
+         * System.out.println("Olá " + nome + ", você tem " + idade + " anos.");
+         * if (idade >= 18) {
+         * System.out.println("Parabéns " + nome + ", você é maior de idade!");
+         * } else {
+         * System.out.println("Desculpe " + nome + ", você é menor de idade!");
+         * }
+         * scanner.close();
+         */
+    }
+}
